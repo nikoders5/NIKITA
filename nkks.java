219b61422d10acf49ac1ea76c4484767093af525
nikoders5 <52191620+nikoders5@users.noreply.github.com>
2019-09-24 17:45:02 +0530

    Create nkks.java

---
+++ nkks.java
@@ -0,0 +1,18 @@
+public class ExampleClass {
+    /**
+     * System.out.println utility method
+     *
+     * @param value : value to print
+     */
+    static void print(String value) {
+        System.out.println(value);
+    }
+    /**
+     * main method for this class
+     */
+    public static void main(String[] args) {
+        while(true){
+            print("printing...");
+        }
+    }
+}
