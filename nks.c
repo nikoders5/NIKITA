35de6326541644c404fe5b904d03de92cfc50e5f
nikoders5 <52191620+nikoders5@users.noreply.github.com>
2019-09-24 17:41:11 +0530

    Create nks.c

---
+++ nks.c
@@ -0,0 +1,14 @@
+
+// C program to illustrate while(1) 
+int main()  
+{ 
+   int i = 0; 
+   while ( 1 )  
+   {   
+      printf( "%d\n", ++i ); 
+      if (i == 5)      
+           break; // Used to come  
+                  // out of loop 
+   } 
+   return 0; 
+} 
