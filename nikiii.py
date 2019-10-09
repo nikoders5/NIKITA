8e13553cdedcd6a855c5fdd9f326fade9368b4ca
nikoders5 <52191620+nikoders5@users.noreply.github.com>
2019-10-09 10:17:26 +0530

    Update nikiii.py

--- nikiii.py
+++ nikiii.py
@@ -6,11 +6,15 @@ print("Hello")
 print("Hello")
 print("Hello")
 print("Hello")
+p
+
+
+
+rint("Hello")
 print("Hello")
 print("Hello")
 print("Hello")
-print("Hello")
-print("Hello")
+print("Hellllo")
 print("Hello")
 print("Hello")
 print("Hello")
